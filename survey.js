6ef49b44f185ac9428e5a4ce4724967a5ef131c2
Kevin Cogen <k.cogen@gmail.com>
2023-03-03 14:58:27 -0500

    profile generator

---
+++ survey.js
@@ -0,0 +1,25 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? Nicknames are also acceptable : ", (answer1) => {
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              console.log(`${answer1} loves ${answer2} while listening to ${answer3}! Their fav meal is ${answer4}, especially if it's ${answer5}!
+Their fav sport is ${answer6} and their superpower is ${answer7}!`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
